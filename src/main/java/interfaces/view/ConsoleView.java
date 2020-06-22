cfe24220f36b70a44e086eb2a07cf1a0befc00a5
Syuparn <s.hello.spagetti@gmail.com>
2020-06-23 08:41:09 +0900

    add readme

--- src/main/java/interfaces/view/ConsoleView.java
+++ src/main/java/interfaces/view/ConsoleView.java
@@ -1,21 +1,36 @@
 package interfaces.view;
 
+import java.util.*;
 import java.util.stream.Stream;
 
 public class ConsoleView {
     public void showCreateToDoItem(String msg) {
+        showHorizontalBar();
         System.out.println(msg);
+        showHorizontalBar();
     }
 
     public void showListToDoItem(Stream<String> lines) {
+        showHorizontalBar();
         lines.forEach(line -> System.out.println(line));
+        showHorizontalBar();
     }
 
     public void showFindToDoItem(Stream<String> lines) {
+        showHorizontalBar();
         lines.forEach(line -> System.out.println(line));
+        showHorizontalBar();
     }
 
     public void showDeleteToDoItem(String msg) {
+        showHorizontalBar();
         System.out.println(msg);
+        showHorizontalBar();
+    }
+
+    private void showHorizontalBar() {
+        int barLength = 30;
+        String barStr = String.join("", Collections.nCopies(barLength, "-"));
+        System.out.println(barStr);
     }
 }
